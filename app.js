1286c027127e27eee851d321f711de38683f220d
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-02 10:26:28 -0800

    update root router;

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ import cors from 'cors';
 
 import gameController from './controllers/gameController.js';
 
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/gameshop';
-const CONNECTION_STRING = 'mongodb://localhost:27017/gameshop';
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/gameshop';
 
 mongoose.connect(CONNECTION_STRING, (err) => {
   if (err) throw console.log({ err });
@@ -15,6 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to GameShop Server-side!');
+});
 gameController(app);
 
 const PORT = process.env.PORT || 4000;
