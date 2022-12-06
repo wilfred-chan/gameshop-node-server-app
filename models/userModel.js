f382f0c66fd50f92b30e66cdc1ebe711e6799d23
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-06 15:17:08 -0800

    design user data model

---
+++ models/userModel.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    firstname: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    lastname: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: 'customer', // can be 'admin' or 'customer'
+    },
+    bio: {
+      type: String,
+      default: 'This gamer is lazy and has not written a bio yet.',
+    },
+    location: {
+      type: String,
+      default: 'Unknown',
+    },
+    image_url: {
+      type: String,
+      default: '/images/avatar/avatar.jpeg',
+    },
+    cart: {
+      type: [String],
+      default: [], // e.g. ['elden-ring', 'cyberpunk-2077']
+    },
+  },
+  {
+    collection: 'users',
+  }
+);
+
+const userModel = mongoose.model('userModel', userSchema);
+export default userModel;
